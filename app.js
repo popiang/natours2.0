6dd35ab7bc875e96395b3b8273a5ca14867140ad
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-06-20 14:58:21 +0800

    52

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const fs = require("fs");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-// 	res.status(200).json({
-// 		message: "Hello from nodejs server side!!",
-// 		app: "Natours"
-// 	});
-// });
-
-// app.post("/", (req, res) => {
-// 	res.send("You can post to this endpoint..");
-// });
+app.use(bodyParser.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-sample.json`));
 
@@ -24,6 +16,25 @@ app.get('/api/v1/tours', (req, res) => {
 			tours: tours
 		}
 	});
+});
+
+app.post("/api/v1/tours", (req, res) => {
+
+	console.log(req.body);
+	const newId = tours[tours.length - 1].id + 1;
+	const newTour = Object.assign({id: newId}, req.body);
+	tours.push(newTour);
+	console.log(newTour);
+
+	fs.writeFile(`${__dirname}/dev-data/data/tours-sample.json`, JSON.stringify(tours), err => {
+		res.status(200).json({
+			status: "success",
+			data: {
+				tour: newTour
+			}
+		});
+	});
+
 });
 
 const port = 3000;
