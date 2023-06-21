4fff919ede758c968d0591cac9d3241d10894e30
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-21 23:42:01 +0800

    94

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,48 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, "A tour must have a name"],
         unique: true,
+		trim: true
     },
-    rating: {
-        type: Number,
-        default: 4.5,
-    },
-    price: {
-        type: Number,
-        required: [true, "A tour must have a price"],
-    },
+	duration: {
+		type: Number,
+		required: [true, "A tour must have a duration"]
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [true, "A tour must have a group size"]
+	},
+	difficulty: {
+		type: String,
+		required: [true, "A tour must have a difficulty"]
+	},
+	ratingsAverage: {
+		type: Number,
+		default: 4.5
+	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0
+	},
+	price: {
+		type: Number,
+		required: [true, "A tour must have a price"],
+	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		trim: true,
+		required: [true, "A tour must have a description"]
+	},
+	imageCover: {
+		type: String,
+		required: [true, "A tour must have an image cover"]
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now()
+	},
+	startDates: [Date]
 });
 
 // use the schema to create model
