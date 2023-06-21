ca39abed1b43b35a20b2ee77413736463642c073
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-21 22:05:16 +0800

    86

--- server.js
+++ server.js
@@ -13,29 +13,42 @@ const DB = process.env.DATABASE.replace(
 console.log(DB);
 
 // create connection
-mongoose
-    .connect(DB, {})
-    .then(() => console.log("DB connection successfull!!"));
+mongoose.connect(DB, {}).then(() => console.log("DB connection successfull!!"));
 
 // create tour schema
 const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "A tour must have a name"],
-		unique: true
-	},
-	rating: {
-		type: Number,
-		default: 4.5
-	},
-	price: {
-		type: Number,
-		required: [true, "A tour must have a price"]
-	}
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        default: 4.5,
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"],
+    },
 });
 
 // use the schema to create model
 const Tour = mongoose.model("Tour", tourSchema);
+
+const newTour = new Tour({
+    name: "The Park Camper",
+    price: 997,
+});
+
+newTour
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => {
+        console.log("Error!!!");
+    });
+
 
 const port = process.env.PORT;
 app.listen(port, () => {
