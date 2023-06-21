ff7df152b8b0a83b79fcbb6d7fed0bdcec4e3cbe
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-21 22:31:55 +0800

    89

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -63,23 +63,23 @@ exports.getTour = (req, res) => {
     });
 };
 
-exports.createTour = (req, res) => {
-    // const newId = tours[tours.length - 1].id + 1;
-    // const newTour = Object.assign({ id: newId }, req.body);
-    // tours.push(newTour);
+exports.createTour = async (req, res) => {
 
-    // fs.writeFile(
-    //     `${__dirname}/dev-data/data/tours-sample.json`,
-    //     JSON.stringify(tours),
-    //     (err) => {
-    //         res.status(200).json({
-    //             status: "success",
-    //             data: {
-    //                 tour: newTour,
-    //             },
-    //         });
-    //     }
-    // );
+	try {
+		const newTour = await Tour.create(req.body);
+		
+		res.status(201).json({
+			status: "success",
+			data: {
+				tour: newTour
+			}
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: "fail",
+			message: error
+		});
+	}
 };
 
 exports.editTour = (req, res) => {
