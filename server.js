42caa5802b06afb6089e40a850bea6fa62ef6567
Shahril Bin Mad Shah <shahril.madshah@airselangor.com>
2023-06-21 17:31:11 +0800

    database

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 const dotenv = require("dotenv");
 
 // to read the variables in the config file and save it in the node js environment variables
-dotenv.config({path: "./config.env"});
+dotenv.config({ path: "./config.env" });
 
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("DB connection successfull!!"));
 
 const port = process.env.PORT;
 app.listen(port, () => {
