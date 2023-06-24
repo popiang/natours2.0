e4ed8d174e353d0867961df2f079b938d535b046
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-24 20:15:00 +0800

    107

--- models/tourModel.js
+++ models/tourModel.js
@@ -49,11 +49,11 @@ const tourSchema = new mongoose.Schema(
             default: Date.now(),
         },
         startDates: [Date],
-		secretTour: {
-			type: Boolean,
-			default: false,
-			select: false
-		}
+        secretTour: {
+            type: Boolean,
+            default: false,
+            select: false,
+        },
     },
     {
         toJSON: { virtuals: true },
@@ -65,6 +65,7 @@ tourSchema.virtual("durationWeeks").get(function () {
     return this.duration / 7;
 });
 
+//* document middleware
 // tourSchema.pre('save', function(next) {
 // 	console.log('Will save the document..');
 // 	next();
@@ -75,16 +76,22 @@ tourSchema.virtual("durationWeeks").get(function () {
 // 	next();
 // });
 
-tourSchema.pre(/^find/, function(next) {
-	this.find({secretTour: {$ne: true}});
-	this.start = Date.now();
-	next();
+//* query middleware
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+    this.start = Date.now();
+    next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
-	console.log(`Query took ${Date.now() - this.start} milliseconds`);
-	console.log(docs);
-	next();
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    console.log(docs);
+    next();
+});
+
+//* aggregate middleware
+tourSchema.pre("aggregate", function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 });
 
 // use the schema to create model
