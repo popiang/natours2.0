0bf98d38f759b64659b97d2f1c7c7229b1243c70
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-25 11:10:30 +0800

    115

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -34,20 +36,9 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-	res.status(400).json({
-		status: "fail",
-		message: `Can't find ${req.originalUrl} on this server`
-	});
+	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-	err.status = err.status || 'error';
-	err.statusCode = err.statusCode || 500;
-
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message
-	});
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
