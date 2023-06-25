98b9d9721709fa0f887a8cfa786cc0a5847bce7b
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-06-25 09:44:37 +0800

    114

--- app.js
+++ app.js
@@ -40,4 +40,14 @@ app.all("*", (req, res, next) => {
 	});
 });
 
+app.use((err, req, res, next) => {
+	err.status = err.status || 'error';
+	err.statusCode = err.statusCode || 500;
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message
+	});
+});
+
 module.exports = app;
