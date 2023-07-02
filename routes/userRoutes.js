1d6f28a267d859d5e1a8bcd947870f0d11fe7af8
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-02 13:00:59 +0800

    126

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
 const router = express.Router();
 
 router
     .route("/")
     .get(userController.getAllUsers)
-    .post(userController.createUser);
+    .post(authController.signup);
 router
     .route("/:id")
     .get(userController.getUser)
