a8740682e3b7488ee854f410a08833870fc84f10
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-02 13:31:17 +0800

    129

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const user = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
 
     res.status(200).json({
         status: "success",
+		token,
         data: {
-            user: user,
+            user: newUser,
         },
     });
 });
