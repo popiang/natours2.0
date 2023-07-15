dd301933aceffca23adb342a4faf17fb1aac22ba
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-16 01:48:22 +0800

    139

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,17 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        if (allowedFields.includes(el)) {
+            newObj[el] = obj[el];
+        }
+    });
+
+    return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
     const users = await User.find();
@@ -7,7 +19,7 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     res.status(200).json({
         status: "success",
         data: {
-			results: users.length,
+            results: users.length,
             users: users,
         },
     });
@@ -40,3 +52,35 @@ exports.deleteUser = (req, res) => {
         message: "This path is not defined yet",
     });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    //* 1) check if there are password and confirmPassword in the req.body
+    if (req.body.password || req.body.confirmPassword) {
+        return next(
+            new AppError(
+                "This route is not for password update. Please use /updatePassword.",
+                400
+            )
+        );
+    }
+
+    //* 2) filter the req.body, only allow name & email fields
+    const filteredBody = filterObj(req.body, "name", "email");
+
+    //* 3) find and update the user
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user.id,
+        filteredBody,
+        {
+            new: true,
+            runValidators: true,
+        }
+    );
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            user: updatedUser,
+        },
+    });
+});
