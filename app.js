7a74af5e25ddea6242a18f6e7aaf6b9e35c6a2f5
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-16 18:55:24 +0800

    143

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
+const rateLimit = require("express-rate-limit");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -12,9 +13,18 @@ const app = express();
 /* middleware */
 
 // http request logger middleware for node.js
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
+
+// rate limiting
+const limiter = rateLimit({
+    max: 100,
+    windowsMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP. Please try again in an hour!",
+});
+
+app.use("/api", limiter);
 
 // to modify the incoming req data and add into req object
 app.use(bodyParser.json());
@@ -36,7 +46,7 @@ app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
-	next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 app.use(globalErrorHandler);
