1b9d4c6f17ad1535ff5c5e296c810471662d95f9
SHAHRIL MAD SHAH <popiang@hotmail.com>
2023-07-22 15:18:12 +0800

    168

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 
 //* must be logged in for all below routes
-router.use(authController.protect); 
+router.use(authController.protect);
 
 router.patch("/updatePassword", authController.updatePassword);
 router.patch("/updateMe", userController.updateMe);
@@ -17,12 +17,16 @@ router.patch("/deleteMe", userController.deleteMe);
 router.get("/me", userController.getMe, userController.getUser);
 
 //* only admin allowed for below routes
-router.use(authController.restrictTo("admin")); 
+// router.use(authController.restrictTo("admin"));
 
-// router
-//     .route("/")
-//     .get(userController.getAllUsers)
+// router.route("/").get(userController.getAllUsers);
 //     .post(userController.createUser);
+
+router
+    .route("/")
+    .get(userController.getAllUsers);
+    // .post(userController.createUser);
+
 router
     .route("/:id")
     .get(userController.getUser)
